aa5388b5c7fdaf9a4fed9d94bc35046820274868
sora9724 <antony.sorokin1@gmail.com>
2024-10-01 18:37:21 +0300

    module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -5,10 +5,13 @@ def get_matrix(n, m, value):
         matrix.append(list_)
         for j in range(1, m+1):
             list_.append(value)
-    return(matrix)
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
+    if m > 0:
+        return(matrix)
+    else:
+        return (list_)
+result1 = get_matrix(1, 2, 10)
+result2 = get_matrix(3, 0, 42)
+result3 = get_matrix(4, 1, 13)
 print(result1)
 print(result2)
 print(result3)

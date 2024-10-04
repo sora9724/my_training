d49aa8b9087e910c3a03bffc0118a3cd236fe2d7
sora9724 <antony.sorokin1@gmail.com>
2024-10-04 09:40:45 +0300

    module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,18 +1,20 @@
 calls = 0
 def count_calls():
     global calls
-    calls =+ 1
+    calls = calls + 1
 
 def string_info(string):
-    return (len(string), string.upper(), string.lower())
     count_calls()
+    return (len(string), string.upper(), string.lower())
 
 def is_contains(string, list_to_search):
+    count_calls()
+    list_to_search = [s.lower() for s in list_to_search]
+    string = string.lower()
     if string in list_to_search:
         return True
     else:
         return False
-    count_calls()
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))

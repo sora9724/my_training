4bf06868b6cc31917c78d811081011263f6bdb18
sora9724 <antony.sorokin1@gmail.com>
2024-10-05 12:02:14 +0300

    module_3_4.py

---
+++ module_3_4.py
@@ -0,0 +1,15 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word = root_word.lower()
+    other_words = [s.lower() for s in other_words]
+    for i in other_words:
+        if i in root_word:
+            same_words.append(i)
+        if root_word in i:
+            same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)

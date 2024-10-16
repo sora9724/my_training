0a6bd3295609ff3a78156ae649f95ef1258dd1dd
sora9724 <antony.sorokin1@gmail.com>
2024-10-16 12:03:50 +0300

    module_4_2.py

--- Module_4_2.py
+++ Module_4_2.py
@@ -3,7 +3,7 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
 
-    inner_function()
+    inner_function() # Функция работает
 
-inner_function()
-test_function()
+inner_function() # Функция не может быть вызванна
+test_function() # Функция работает

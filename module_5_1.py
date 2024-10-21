c431ab2b014ab74c0f8a00a79612ae25b1be87e6
sora9724 <antony.sorokin1@gmail.com>
2024-10-21 11:30:19 +0300

    module_5_1.py

---
+++ module_5_1.py
@@ -0,0 +1,21 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+        print(f'{name}, {number_of_floor}')
+
+    def go_to(self, new_floor):
+        if new_floor <= 0:
+            print("Такого этажа не существует")
+        for floor in range(1, new_floor + 1):
+            if new_floor > self.number_of_floor:
+                print('Такого этажа не существует')
+                break
+            else:
+                print(floor)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(0)

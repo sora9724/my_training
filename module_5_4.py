c8bb626ecdf1b904b253cf8c41129432b5016860
sora9724 <antony.sorokin1@gmail.com>
2024-11-06 22:08:40 +0300

    module_5_4.py

---
+++ module_5_4.py
@@ -0,0 +1,31 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floor}'
+
+    houses_history = []
+
+    def  __new__(cls, *args, **kwargs):
+        return cls.houses_history.append(args[0])
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)

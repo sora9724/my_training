e95d6d4356b6301036aec7d24b4ca4b13a8b8777
sora9724 <antony.sorokin1@gmail.com>
2024-11-15 00:24:21 +0300

    module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,41 +1,39 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
+#Класс животного с функцией поедания растений
+class Animal:     
     alive = True
     fed = False
 
-class Plant:
     def __init__(self, name):
         self.name = name
+
+    def eat(self, food):
+        if food.edible == True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+            
+#Класс растения
+class Plant:    
     edible = False
+    def __init__(self, name):
+        self.name = name
 
 class Flower(Plant):
     edible = False
 
 class Fruit(Plant):
     edible = True
-  
-#Действия млекопитающего
-class Mammal(Animal):
-    def eat(self, food):
-        if Plant.edible == False:
-            print(f'{self.name} съел {food.name}')
-            Animal.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            Animal.alive = False
-          
-# Действия хищника
-class Predator(Animal):
-    def eat(self, food):
-        if Plant.edible == True:
-            print(f'{self.name} съел {food.name}')
-            Animal.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            Animal.alive = False
 
-#Блок проверки
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+
+# Блок проверки
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')

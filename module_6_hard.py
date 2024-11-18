316210a98c7c3a5b08b3f14e71e2ccbff7abdb0b
sora9724 <antony.sorokin1@gmail.com>
2024-11-18 23:23:46 +0300

    module_6_hard.py

--- module_6_hard.py
+++ module_6_hard.py
@@ -7,7 +7,7 @@ class Figure:
         self.__sides = __sides
 
     def get_color(self):
-        print (self.__color)
+        return self.__color
 
     def __is_valid_color(self, r, g, b):
         if r in range(0, 256) and g in range(0, 256) and b in range(0, 256):
@@ -19,7 +19,7 @@ class Figure:
         if self.__is_valid_color(r, g, b) == True:
             self.__color = [r, g, b]
         else:
-            self.__color = self.__color
+            self.__color = list(self.__color)
 
     def __is_valid_sides(self, *args):
         if args > 0 and args == int and self.side_count == len(args):
@@ -28,41 +28,38 @@ class Figure:
             return False
 
     def get_sides(self):
-        print(self.__sides)
+        return self.__sides
 
     def __len__(self):
-        print (self.__sides * self.side_count)
+        if self.side_count > 1:
+            return self.__sides * self.side_count
+        else:
+            return self.__sides[0]
 
     def set_sides(self, *new_sides):
-        if len(new_sides) != self.side_count:
-            new_sides = self.side_count
-            return new_sides
-        else:
-            self.side_count = self.side_count
-
+        new_sides = list(new_sides)
+        if self.side_count != len(new_sides):
+            self.__sides = [self.__sides] * self.side_count
+        if self.side_count == len(new_sides):
+            self.__sides = new_sides
+        if len(new_sides) == 1:
+            self.__sides = new_sides * self.side_count
 
 
 class Circle(Figure):
-    sides_count = 1
-
     def __init__(self, __color, __sides):
         super().__init__(__color, __sides)
         self.__sides = __sides
-
-    def radius(self):
-        __radius = self.__sides / (2 * 3.14)
-        return __radius
+        self.side_count = 1
 
     def get_square(self):
-        s = (self.__radius * 3.14) ** 2
+        __radius = self.__sides / (2 * 3.14)
+        s = (__radius * 3.14) ** 2
         return s
-
-    def __len__(self):
-        return self.__sides
 
 
 class Triangle(Figure):
-    sides_count = 3
+    side_count = 3
 
     def __init__(self, __color, __sides):
         super().__init__(__color, __sides)
@@ -80,12 +77,9 @@ class Cube(Figure):
         super().__init__(__color, __sides)
         self.__sides = __sides
 
-
     def get_volume(self):
         v = self.__sides ** 3
         return v
-
-
 
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
@@ -108,3 +102,4 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
+
